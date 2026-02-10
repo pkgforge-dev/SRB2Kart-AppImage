429fdc9ecca153e925d89cb5ee197ec1e4a84c1b
Link4Electronics <link4electronics@gmail.com>
2026-02-09 23:28:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,19 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    glu        \
+    libdecor   \
+    sdl2       \
+    sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package srb2kart-data
+make-aur-package srb2kart
 
 # If the application needs to be manually built that has to be done down here
 
