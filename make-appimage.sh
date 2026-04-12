0cbe774912e46ee59863e5f1faa3dfe272bdb659
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:06:39 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,24 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q srb2kart | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export APPNAME=SRB2Kart
-export ICON=https://git.do.srb2.org/KartKrew/Kart-Public/-/raw/next/srb2.png?ref_type=heads
-export DESKTOP=/usr/share/applications/srb2kart-opengl.desktop
-export STARTUPWMCLASS=srb2kart
+export ICON=https://raw.githubusercontent.com/STJr/Kart-Public/refs/heads/master/srb2.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/srb2kart
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/srb2kart
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# test the final app
-#quick-sharun --test ./dist/*.AppImage
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
