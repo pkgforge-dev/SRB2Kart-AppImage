34fa049587cf584eddb00030b231bf0969692451
Link4Electronics <link4electronics@gmail.com>
2026-08-25 21:11:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q srb2kart | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q srb2kart | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -17,10 +17,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/srb2kart
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# test the final app
-#quick-sharun --test ./dist/*.AppImage
