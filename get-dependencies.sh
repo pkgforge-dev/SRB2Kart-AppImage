9e82cca9ff5b3e7c3b698ebf7d3b4b96530fdba1
Link4Electronics <link4electronics@gmail.com>
2026-08-30 10:20:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -38,6 +38,6 @@ mkdir -p ./AppDir/bin
 mkdir -p ./AppDir/share/games/SRB2Kart
 bsdtar -xvf AssetsLinuxOnly.zip -C ./AppDir/share/games/SRB2Kart
 
-cd ./SRB2Kart
+cd ./SRB2Kart/src
 make LINUX64=1 HAVE_DISCORDRPC=1 -j$(nproc)
-mv -v /bin/Linux64/Release/lsdl2srb2kart ../AppDir/bin/srb2kart
+mv -v ../bin/Linux64/Release/lsdl2srb2kart ../../AppDir/bin/srb2kart
