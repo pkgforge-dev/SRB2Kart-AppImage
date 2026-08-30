3f2c5fe33885108f75f09c00c916492068519ada
Link4Electronics <link4electronics@gmail.com>
2026-08-30 10:17:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,11 +26,18 @@ make-aur-package discord-rpc
 echo "Building stable version of SRB2Kart..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/STJr/Kart-Public"
-VERSION="$(curl -s https://api.github.com/repos/STJr/Kart-Public/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
-git clone "$REPO" ./SRB2Kart
+VERSION="$(curl -sL https://api.github.com/repos/STJr/Kart-Public/releases/latest | grep '"tag_name"' | head -1 | cut -d '"' -f 4)"
+git clone --branch "$VERSION" "$REPO" ./SRB2Kart
 echo "$VERSION" > ~/version
 
+# AssetsLinuxOnly.zip from the same latest tag
+curl -L -o AssetsLinuxOnly.zip \
+  "https://github.com/STJr/Kart-Public/releases/download/$VERSION/AssetsLinuxOnly.zip"
+
 mkdir -p ./AppDir/bin
+mkdir -p ./AppDir/share/games/SRB2Kart
+bsdtar -xvf AssetsLinuxOnly.zip -C ./AppDir/share/games/SRB2Kart
+
 cd ./SRB2Kart
 make LINUX64=1 HAVE_DISCORDRPC=1 -j$(nproc)
 mv -v /bin/Linux64/Release/lsdl2srb2kart ../AppDir/bin/srb2kart
